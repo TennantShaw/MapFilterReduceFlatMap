00c4904ca03deb893a97639cc4ac9779e9285354
Tennant Shaw <tennantshaw@macs-mbp.lan>
2017-04-11 17:03:02 -0400

    Initial Commit

---
+++ MapFilterReduceFlatMap20170411/MapFilterReduceFlatMap20170411.h
@@ -0,0 +1,19 @@
+//
+//  MapFilterReduceFlatMap20170411.h
+//  MapFilterReduceFlatMap20170411
+//
+//  Created by Tennant Shaw on 4/11/17.
+//  Copyright © 2017 Tennant Shaw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for MapFilterReduceFlatMap20170411.
+FOUNDATION_EXPORT double MapFilterReduceFlatMap20170411VersionNumber;
+
+//! Project version string for MapFilterReduceFlatMap20170411.
+FOUNDATION_EXPORT const unsigned char MapFilterReduceFlatMap20170411VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <MapFilterReduceFlatMap20170411/PublicHeader.h>
+
+
